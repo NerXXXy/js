29f253751a98ec6c7f19e4792bd2977844a98c2e
Volodymyr Feduniv <57459268+NerXXXy@users.noreply.github.com>
2021-11-11 10:36:17 +0200

    Add files via upload

---
+++ H1JS/script.js
@@ -0,0 +1,91 @@
+// Task 1
+//
+// let word1 = 'hello';
+// let word2 = 'owu';
+// let word3 = 'com';
+// let word4 = 'ua';
+// let n1 = 1;
+// let n2 = 10;
+// let n3 = -999;
+// let n4 = 123;
+// let n5 = 3.14;
+// let n6 = 2.7;
+// let n7 = 16;
+// let bool1 = true;
+// let bool2 = false;
+
+
+// Task 2
+//
+// let word1 = 'hell';
+//  let word2 = 'ou';
+//  let word3 = 'cm';
+//  let word4 = 'a';
+//  let n1 = 10;
+//  let n2 = 1;
+//  let n3 = -99;
+//  let n4 = 13;
+//  let n5 = 314;
+//  let n6 = 27;
+//  let n7 = 1;
+//  let bool1 = false;
+//  let bool2 = true;
+
+
+// Task 3
+//
+// let firstName = 'Volodymyr';
+// let middleName = 'Borysovych';
+// let lastName = 'Feduniv';
+//
+// let person = lastName + ' ' + firstName + ' ' + middleName;
+
+
+// Task 4
+//
+// let name = prompt('Name');
+// let fatherName = prompt('your fathers name');
+// let age = +prompt('How old are you?');
+//
+// console.log(`Hello ${name} ${fatherName}. You are ${age} years old`);
+
+
+// Task 5
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+// Task 6
+//
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 == 6 -> false
+// 5 <= 6 -> false
+// 10 == 10 -> true
+// 10 === 10 -> true
+// 10 > 10 -> false
+// 10 < 10 -> false
+// 10 !== 10 -> false
+// 123 === '123' -> false
+// 123 == '123' -> true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
