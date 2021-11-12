6afa191d336e066ce99ee6f83f97f7e7356c5dfb
Volodymyr Feduniv <57459268+NerXXXy@users.noreply.github.com>
2021-11-12 11:35:35 +0200

    Add files via upload

---
+++ P1JS/script.js
@@ -0,0 +1,81 @@
+// Task 1
+//
+//function randomMas (n){
+//     let mas = [];
+//     let sum = 0 ;
+//     for (let i = 0 ; i < n ; i++){
+//         mas.push(Math.floor(Math.random()*(10 - 1)))
+//     }
+//     for (let j = 0 ; j < mas.length; j++){
+//         sum += mas[j];
+//     }
+//     return console.log(sum);
+// }
+//
+// randomMas(3);
+
+
+// Task 2
+//
+// let brook = {
+//     name : 'book',
+//     pages : '10',
+//     genre : 'myth'
+// }
+
+
+// Task 3
+//
+// let brook = {
+//     name : 'book',
+//     pages : '20',
+//     genre : 'roman',
+//     writer : 'Roku'
+// }
+
+
+// Task 4
+//
+// let mas = [{name : 'Petya', pages:'20', genre : 'Roman', writer : 'Leonid' },{name: 'Dungeon Master', pages: '10', genre: 'gachi', writer: 'Billy'}]
+//
+//
+// function print (massive){
+//     for (let i = 0 ; i < massive.length; i++){
+//         console.log(massive[i]);
+//     }
+// }
+//
+// print (mas);
+//
+
+
+// Task 5
+//
+// let h = 23;
+// let w = 10;
+// let s = h * w;
+// console.log(s);
+
+
+// Task 6
+//
+// let h = 10;
+// let d = 4;
+// let v = 3.14*Math.pow(d,2)/4*h;
+// console.log(v);
+//
+
+
+// Task 7
+//
+// let n = 3;
+// let m = 4;
+// let k = Math.sqrt((n*n)+(m*m));
+// console.log(k);
+
+
+
+
+
+
+
