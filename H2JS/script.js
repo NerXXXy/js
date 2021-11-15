f24c31682b13db267b852499f3030fb0686079f8
Volodymyr Feduniv <57459268+NerXXXy@users.noreply.github.com>
2021-11-15 13:00:37 +0200

    Add files via upload

---
+++ H2JS/script.js
@@ -0,0 +1,134 @@
+// Task 1
+//
+// function t (time){
+//     if (time <= 15){
+//         return '1 частина години';
+//     }
+//     else if (time > 15 && time <= 30){
+//         return '2 частина години ';
+//     }
+//     else if (time > 30 && time <= 45){
+//        return '3 частина години';
+//     }
+//     else if (time > 45 && time <= 60){
+//         return '4 частина години'
+//     }
+//     else {
+//         return 'введено неправильне значення'
+//     }
+// }
+// console.log(time(60))
+
+
+// Task 2
+//
+// function decade (day){
+//     if (day >= 1 && day < 11){
+//         return '1 декада'
+//     }
+//     else if (day >= 11 && day < 21){
+//         return '2 декада'
+//     }
+//     else if (day >=21 && day < 32){
+//         return '3 декада'
+//     }
+//     else {
+//         return 'введено неправильне значення'
+//     }
+// }
+// console.log(decade(13));
+
+
+// Task 3
+//
+//function bool (test){
+//     if (test === true){
+//         return 'Вірно'
+//     }
+//     else if (test === false){
+//         return 'Невірно'
+//     }
+//     else {
+//         return 'Введено неправильне значення'
+//     }
+// }
+// console.log(bool(true));
+
+
+// Task 4
+//
+// function NotZero (n){
+//     if (n !== 0){
+//         return 'Невірно'
+//     }
+//     else {
+//         return 'Вірно'
+//     }
+// }
+// console.log(NotZero(1));
+// console.log(NotZero(0));
+// console.log(NotZero(-3));
+
+
+// Task 5
+//
+// function plans (day){
+//     switch (day){
+//         case 1:
+//             console.log('Понеділок : Плавання');
+//             break;
+//
+//         case 2:
+//             console.log('Вівторок : Навчання');
+//             break;
+//
+//         case 3:
+//             console.log('Середа : Поїзка в гори');
+//             break;
+//
+//         case 4:
+//             console.log('Четвер : Зустріч з друзями');
+//             break;
+//
+//         case 5:
+//             console.log('Пятниця : Відпочинок від відпочинку');
+//             break;
+//
+//         case 6:
+//             console.log('Субота : Відпочинок від пятниці');
+//             break;
+//
+//         case 7:
+//             console.log('Неділя : Піти в хрестовий похід і взяти єрусалим');
+//             break;
+//     }
+// }
+// plans(7)
+
+
+// Task 6
+//
+// function year (y){
+//     if (y % 4 === 0){
+//         return 'Високосний';
+//     }
+//     else {
+//         return 'Не високосний'
+//     }
+// }
+// console.log(year(364));
+
+
+// Task 7
+//
+// function question (q){
+//     if (q === 'ECMAScript'){
+//         return 'Правильно!'
+//     }
+//     else {
+//         return 'Не знаєте? ECMAScript!'
+//     }
+// }
+// console.log(question('ECMAScript'));
+
+
