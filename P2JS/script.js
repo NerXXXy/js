1ca981a9fcf2b91b79e3cd051023dd73ef2443fa
Volodymyr Feduniv <57459268+NerXXXy@users.noreply.github.com>
2021-11-16 10:49:39 +0200

    Add files via upload

---
+++ P2JS/script.js
@@ -0,0 +1,118 @@
+// Task 1
+//
+// function bigger (n1,n2){
+//     if (n1 > n2){
+//         return n1;
+//     }
+//     else if (n2 > n1){
+//         return n2
+//     }
+//     else {
+//         return 'Вони рівні'
+//     }
+// }
+// console.log(bigger(4,4));
+
+
+// Task 2
+//
+// let number = +prompt("Введіть номер квартири");
+// function house (n){
+//     if (n > 1 && n <=20){
+//         return 1;
+//     }
+//     else if ( n > 20 && n <= 48){
+//         return 2
+//     }
+//     else if (n > 48 && n <= 90){
+//         return 3
+//     }
+//     else {
+//        return 'Такої квартири нема';
+//     }
+// }
+//
+// console.log(house(number));
+
+
+//  Task 3
+//
+// function rightNumber (number){
+//     if (number === 10){
+//         return 'Вірно'
+//     }
+//     else {
+//         return 'Невірно'
+//     }
+// }
+// console.log(rightNumber(10));
+
+
+
+// Task 4
+//
+// function type (x){
+//     if (typeof x === 'number'){
+//         return 1
+//     }
+//     else if (typeof x === 'string'){
+//         return 2
+//     }
+//     else if (typeof x === 'boolean'){
+//         return 3
+//     }
+//     else if (typeof x === 'object'){
+//         return 3
+//     }
+//     else {
+//         return 'else'
+//     }
+// }
+// console.log(type('hello'));
+
+
+
+// Task 5
+//
+// function temperature (t){
+//     if (t >= 10 && t <= 22){
+//         return 'ми йдемо ВЧИТИСЯ'
+//     }
+//     else {
+//         return 'сидимо вдома і вчимося ОНЛАЙН'
+//     }
+// }
+// console.log(temperature(9));
+
+
+
+// Task 6
+//
+// function quiz (n){
+//     switch (n){
+//         case 1:
+//             console.log('Car');
+//             break
+//
+//         case 2:
+//             console.log('Moto');
+//             break
+//
+//         case 3:
+//             console.log('Iphone');
+//             break
+//
+//         case 4:
+//             console.log('PC');
+//             break
+//
+//         case 5:
+//             console.log('Nothing');
+//             break
+//     }
+// }
+// quiz(3);
+
+
+
+
